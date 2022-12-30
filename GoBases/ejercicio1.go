a699909fc3eca31ffc28bef3f53bd0c42870882b
Jessica Sierra Saenz <jessica.sierra@mercadolibre.com.co>
2022-12-30 17:42:41 -0500

    archivo nombre agregado

---
+++ GoBases/ejercicio1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	nombre := "Jessica Sierra Sáenz"
+	direccion := "DG 58 # 45-44"
+
+	fmt.Printf("Mi nombre es: %s y mi dirección es: %s\n", nombre, direccion )
+}
+
+/*
+Ejercicio 1 - Imprimí tu nombre
+Tendrás que: 
+Crear una aplicación donde tengas como variable tu nombre y dirección.
+Imprimir en consola el valor de cada una de las variables.
+*/
